6bb79f0dc64fb3555ab20bdcafa5542e98375fc8
raghavbahukhandi <raghapb34n@gmail.com>
2022-05-07 00:35:34 +0530

    created users model and table

--- Gemfile
+++ Gemfile
@@ -12,11 +12,14 @@ gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-rails'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,47 @@
 class User < ApplicationRecord
+    attr_reader :password
+
+    validates :username, uniqueness: true, presence: true 
+    validates :password_digest, presence: true 
+    validates :session_token, presence: true 
+    validates :password, length: { minimum: 6, allow_nil: true }
+    before_validation :ensure_session_token 
+
+    after_initialize do |user| 
+        user.session_token ||= User.generate_session_token
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username).first 
+
+        return nil if user.nil? 
+
+        user.is_password?(password) ? user : nil
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end 
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        return self.session_token
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    private 
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end 
 end
